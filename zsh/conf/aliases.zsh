204ad7b2c7358623a84edf85af3d7c95467055af
Simon Thulbourn <simon+github@thulbourn.com>
2016-05-18 17:15:35 +0100

    remove lock alias

--- zsh/conf/aliases.zsh
+++ zsh/conf/aliases.zsh
@@ -6,8 +6,6 @@ function md5() {
   echo -n $@ | openssl md5
 }
 
-alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
-
 alias yj="ruby -rjson -ryaml -e \"puts YAML.load(STDIN.read).to_json\""
 
 function weather() {
