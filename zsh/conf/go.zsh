2389af785c0892683a9e25b8bb9fd9d94d28ef8a
Simon Thulbourn <simon+github@thulbourn.com>
2016-05-18 17:15:46 +0100

    Add external dir to GOPATH

--- zsh/conf/go.zsh
+++ zsh/conf/go.zsh
@@ -1,6 +1,6 @@
 export GOROOT="/usr/local/opt/go/libexec"
 export GOHOME="$HOME/Developer/go"
-export GOPATH="$HOME/Developer/go"
-export PATH="$GOPATH/bin:$PATH"
+export GOPATH="$HOME/Developer/go/external:$HOME/Developer/go/internal"
+export PATH="$HOME/Developer/go/external/bin:$HOME/Developer/go/internal/bin:$PATH"
 
 export GO15VENDOREXPERIMENT=1
